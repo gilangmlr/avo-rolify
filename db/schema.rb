96274f23b2104dbe3e549668c108dadd1aea7ed5
Gilang Gumilar <gilangmlr@gmail.com>
2022-07-10 21:03:30 +0800

    Add Forum model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_10_112947) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_10_130143) do
+  create_table "forums", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "roles", force: :cascade do |t|
     t.string "name"
     t.string "resource_type"
